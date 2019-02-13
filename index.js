903691d1c1fd4b5c88bef400e4d62fffa87c5870
Kyle Brown <kylelibrown@gmail.com>
2019-02-13 20:27:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function  happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  return 'It\s 12 days until Valentine\'s day!'
+  return 'It\s 20 days until Mother\'s day!'
 }
